580a89fca8d1a738bb26d343d5d83aefcb79ff91
Kazuyuki Honda <hakobera@gmail.com>
2011-03-20 14:50:14 +0900

    Firefox 4 でサンプル音を再生できるようにした。また、デフォルトの値を C0 から A4 に変更。

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var i, option,
         noteSelect = document.getElementById('note'),
         octaveSelect = document.getElementById('octave'),
-        pitchInfoText = document.getElementById('pitchinfo');
+        pitchInfoText = document.getElementById('pitchInfo'),
+        playButton = document.getElementById('play');
 
     var noteNames = PitchTable.getNoteNames(),
         noteNamesLength = noteNames.length,
@@ -25,19 +26,52 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     octaveSelect.appendChild(octaveOptions);
 
-    // Bind events
-    var calcPitch = function() {
+    // Event function
+
+    /**
+     * Current pitch information.
+     */
+    var audio = new Audio(),
+        sampleRate = 44100,
+        soundDataSize = 2/*secounds*/ * sampleRate,
+        soundData = new Float32Array(soundDataSize),
+        pitch;
+
+    function calcPitch(e) {
         var note = noteSelect.value;
         var octave = octaveSelect.value;
-        var pitch = PitchTable.calcPitch(note, octave);
-        var result = 'KeyB. Note = ' + pitch.noteName + ', MIDI Note = ' + pitch.noteNumber + ', Freq = ' + pitch.frequency.toFixed(3) + ' Hz';
-        pitchInfoText.value = result;
+        pitch = PitchTable.calcPitch(note, octave);
+        pitchInfoText.value = 'KeyB. Note = ' + pitch.noteName + ', MIDI Note = ' + pitch.noteNumber + ', Freq = ' + pitch.frequency.toFixed(3) + ' Hz';
+    }
+
+    function createSoundData() {
+       var freq = pitch.frequency,
+           k = 2 * Math.PI * freq / sampleRate,
+           i;
+
+       for (i = 0; i < soundDataSize; ++i) {
+           soundData[i] = 0.1 * Math.sin(k * i);
+       }
+
+       return soundData;
+    }
+
+    function playSound(e) {
+        e.preventDefault();
+        if (audio.mozSetup) {
+            audio.mozSetup(1, sampleRate);
+            audio.mozWriteAudio(createSoundData());
+        }
     };
 
+    // Bind events
     noteSelect.addEventListener('change', calcPitch, false);
     octaveSelect.addEventListener('change', calcPitch, false);
+    playButton.addEventListener('click', playSound, false);
 
     // Calc initial value
+    noteSelect.value = 'A';
+    octaveSelect.value = '4';
     calcPitch();
     
 }, false);
